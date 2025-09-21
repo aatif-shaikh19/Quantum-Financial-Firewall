a69c2346ba3e30db74fe95a395363a3a6f6226b1
Ariba-Khan4 <khariba436@gmail.com>
2025-09-21 19:45:04 +0530

    Update kafka_stream.py

--- backend/ingestion/kafka_stream.py
+++ backend/ingestion/kafka_stream.py
@@ -1,1 +1,16 @@
-#kafka
+from kafka import KafkaConsumer
+import json
+
+def consume_transactions():
+    consumer = KafkaConsumer(
+        'transactions',
+        bootstrap_servers=['localhost:9092'],
+        auto_offset_reset='earliest',
+        value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+    )
+
+    for message in consumer:
+        tx = message.value
+        print(f"[Kafka Stream] New Transaction: {tx}")
+        yield tx
+

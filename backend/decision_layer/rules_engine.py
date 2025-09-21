c8314f40516377c352c5c731ef035a02d982889b
Ariba-Khan4 <khariba436@gmail.com>
2025-09-21 19:36:15 +0530

    Update rules_engine.py

--- backend/decision_layer/rules_engine.py
+++ backend/decision_layer/rules_engine.py
@@ -1,1 +1,8 @@
+def apply_rules(tx, risk_score):
+    if risk_score > 75:
+        return "BLOCKED"
+    elif 40 < risk_score <= 75:
+        return "FLAGGED - REVIEW"
+    else:
+        return "APPROVED"
 

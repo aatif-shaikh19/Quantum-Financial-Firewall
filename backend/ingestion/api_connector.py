ee348c1a8fc66aed7ce76d32632280dceb74374c
Ariba-Khan4 <khariba436@gmail.com>
2025-09-21 19:46:35 +0530

    Update api_connector.py

--- backend/ingestion/api_connector.py
+++ backend/ingestion/api_connector.py
@@ -1,1 +1,11 @@
+import requests
+
+def get_crypto_price(symbol="ETH"):
+    try:
+        url = f"https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
+        r = requests.get(url)
+        data = r.json()
+        return {"symbol": symbol, "price_usd": data["ethereum"]["usd"]}
+    except Exception as e:
+        return {"error": str(e)}
 

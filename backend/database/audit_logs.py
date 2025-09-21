a8d54d3803271a5dcd777472a9fa6ea584f914ba
Ariba-Khan4 <khariba436@gmail.com>
2025-09-21 19:38:20 +0530

    Update audit_logs.py

--- backend/database/audit_logs.py
+++ backend/database/audit_logs.py
@@ -1,1 +1,13 @@
+import json
+from datetime import datetime
+
+def save_log(tx, risk_score, decision):
+    log_entry = {
+        "timestamp": str(datetime.utcnow()),
+        "transaction": tx,
+        "risk_score": risk_score,
+        "decision": decision
+    }
+    with open("audit_logs.json", "a") as f:
+        f.write(json.dumps(log_entry) + "\n")
 

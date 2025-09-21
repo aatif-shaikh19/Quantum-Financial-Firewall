d7b346037750f47b5e6e4dc5d902dd0435fba892
Ariba-Khan4 <khariba436@gmail.com>
2025-09-21 19:47:41 +0530

    Update neo4j_connector.py

--- backend/database/neo4j_connector.py
+++ backend/database/neo4j_connector.py
@@ -1,1 +1,18 @@
+from neo4j import GraphDatabase
+
+class FraudGraphDB:
+    def __init__(self, uri="bolt://localhost:7687", user="neo4j", password="test"):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        self.driver.close()
+
+    def log_relationship(self, sender, receiver):
+        with self.driver.session() as session:
+            session.run(
+                "MERGE (a:Wallet {id:$sender}) "
+                "MERGE (b:Wallet {id:$receiver}) "
+                "MERGE (a)-[:SENT_TO]->(b)",
+                sender=sender, receiver=receiver
+            )
 
